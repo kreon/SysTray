6e2e84fc40ad4c1c93e2ab4c7ca90805eb76ed61
kreon <41023591+kreon@users.noreply.github.com>
2018-11-06 20:18:54 +0300

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,48 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SysTray.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SYSTRAYDEMO_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDS_APPTOOLTIP                  104
+#define IDM_EXIT                        105
+#define IDI_SYSTRAYDEMO                 107
+#define IDI_SYSTRAY                     107
+#define IDI_SMALL                       108
+#define IDC_SYSTRAYDEMO                 109
+#define IDC_SYSTRAY                     109
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define ID_S_                           32771
+#define ID_S_EXIT                       32772
+#define ID_SYSTRAYMENU_                 32773
+#define ID_SYSTRAYMENU_ENABLE           32774
+#define ID_SYSTRAYMENU_DISABLE          32775
+#define ID_SYSTRAYMENU_TEST1            32776
+#define ID_SYSTRAYMENU_TEST2            32777
+#define IDM_DISABLE                     32778
+#define IDM_ENABLE                      32779
+#define IDM_TEST2                       32780
+#define IDM_TEST1                       32781
+#define ID_SYSTRAYMENU_SEP              32782
+#define IDM_SEP                         32783
+#define ID_SYSTRAYMENU_32784            32784
+#define IDM_MAX_POWSAV                  32785
+#define IDM_BAL_POWSAV                  32786
+#define IDM_MIN_POWSAV                  32787
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32788
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
